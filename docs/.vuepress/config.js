a42a5937910f9506689c06af8e06b79ec72f2c61
Sww-echo <2560910707@qq.com>
2020-11-17 21:17:09 +0800

    第一次提交我的博客1.0

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,48 @@
+module.exports = {
+  title: '天外来物',// 配置标题
+  description: '我的个人网站',//  描述
+  head: [ // 注入到当前页面的 HTML <head> 中的标签
+    [
+      'link', {
+        rel: 'icon',
+        href: '/002.jpg'
+      }
+
+    ] // 增加一个自定义的 favicon(网页标签的图标)
+  ],
+  base: '/blog/', // 这是部署到github相关的配置
+  markdown: {
+    lineNumbers: true // 代码块显示行号
+  },
+  themeConfig: {
+    sidebarDepth: 3,
+    nav: [ // 导航栏配置
+      {
+        text: '博客',
+        link: '/blog/'
+      },
+      {
+        text: '博客',
+        link: '/blog/'
+      },
+      {
+        text: '博客',
+        link: '/blog/'
+      },
+      {
+        text: '博客',
+        link: '/blog/'
+      }
+    ],
+    sidebar: {//  配置侧边栏
+      '/blog/': [{
+        title: '文档',
+        collapsable: false,
+        children: [
+          'document/vuepress',
+        ]
+      }]
+
+    }
+  }
+};
